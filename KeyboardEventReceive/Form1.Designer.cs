2c5e905808118017e61f8d0f77fc500a287682a9
mickey <mickey@arbor.com.tw>
2022-08-22 12:00:37 +0800

    first commit

---
+++ KeyboardEventReceive/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace KeyboardEventReceive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_IP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2_PORT = new System.Windows.Forms.TextBox();
+            this.button1_startService = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1_IP
+            // 
+            this.textBox1_IP.Location = new System.Drawing.Point(395, 149);
+            this.textBox1_IP.Name = "textBox1_IP";
+            this.textBox1_IP.Size = new System.Drawing.Size(227, 36);
+            this.textBox1_IP.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // textBox2_PORT
+            // 
+            this.textBox2_PORT.Location = new System.Drawing.Point(395, 202);
+            this.textBox2_PORT.Name = "textBox2_PORT";
+            this.textBox2_PORT.Size = new System.Drawing.Size(227, 36);
+            this.textBox2_PORT.TabIndex = 2;
+            // 
+            // button1_startService
+            // 
+            this.button1_startService.Location = new System.Drawing.Point(298, 258);
+            this.button1_startService.Name = "button1_startService";
+            this.button1_startService.Size = new System.Drawing.Size(324, 41);
+            this.button1_startService.TabIndex = 4;
+            this.button1_startService.Text = "Start";
+            this.button1_startService.UseVisualStyleBackColor = true;
+            this.button1_startService.Click += new System.EventHandler(this.button1_startService_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1_startService);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2_PORT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1_IP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_IP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2_PORT;
+        private System.Windows.Forms.Button button1_startService;
+    }
+}
+
---
+++ KeyboardEventReceive/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace KeyboardEventReceive
+{
+    public partial class Form1 : Form
+    {
+
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL")]
+        public static extern IntPtr FindWindow(string lpClassName,
+        string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        String SERVER_IP = "192.168.1.109";
+        int PORT_NO = 13000;
+        bool isServiceStart = false;
+        Thread tcpThread = null;
+        TcpListener listener = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Load_Setting();
+            //tcpServer();
+        }
+
+        private void Load_Setting()
+        {
+            // loading save data
+            //SERVER_IP = Properties.Settings.Default.SERVER_IP;
+            SERVER_IP = GetLocalIPAddress();
+            PORT_NO = Properties.Settings.Default.PORT_NO;
+
+            // update UI contain
+            textBox1_IP.Text = SERVER_IP;
+            textBox2_PORT.Text = PORT_NO.ToString();
+
+            // thread
+            tcpThread = new Thread(tcpServer);
+        }
+
+        private void ReLoad_Setting()
+        {
+            if(!SERVER_IP.Equals(textBox1_IP.Text))
+            {
+                SERVER_IP = textBox1_IP.Text;
+                Console.WriteLine("current input IP not equal dafault, so changed it : " + SERVER_IP);
+            }
+            if (!PORT_NO.ToString().Equals(textBox2_PORT.Text))
+            {
+                int i = int.Parse(textBox2_PORT.Text);
+                PORT_NO = i;
+                Console.WriteLine("current input PORT not equal dafault, so changed it : " + PORT_NO);
+            }
+        }
+
+
+        private void tcpServer()
+        {
+            try
+            {
+                //---listen at the specified IP and port no.---
+                System.Net.IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+                //TcpListener listener = new TcpListener(localAdd, PORT_NO);
+                listener = new TcpListener(localAdd, PORT_NO);
+                Console.WriteLine("Listening...");
+                listener.Start();
+                while (isServiceStart)
+                {
+                //---incoming client connected---
+                TcpClient client = listener.AcceptTcpClient();
+
+                //---get the incoming data through a network stream---
+                NetworkStream nwStream = client.GetStream();
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                //---read incoming stream---
+                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //---convert the data received into a string---
+                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received : " + dataReceived);
+
+                // send keyboard event
+                SendKeys.SendWait(dataReceived);
+
+                //---write back the text to the client---
+                /*
+                Console.WriteLine("Sending back : " + dataReceived);
+                nwStream.Write(buffer, 0, bytesRead);
+                */
+                client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                isServiceStart = false;
+                listener.Stop();
+                Console.WriteLine("listener stop!!");
+                Console.WriteLine("thread state : " + tcpThread.ThreadState);
+
+                if(!SERVER_IP.Equals(GetLocalIPAddress()))
+                {
+                    this.Invoke(new Action(() => { MessageBox.Show(this, "無效的IP, 請確認是否與主機IP一致");}));
+                    this.Invoke((MethodInvoker)delegate {
+                        button1_startService.Text = "Start";
+                    });
+                }
+            }
+            //listener.Stop();
+            //Console.WriteLine("listener stop!!");
+            Console.ReadLine();
+        }
+
+        private void button1_startService_Click(object sender, EventArgs e)
+        {
+            switch (tcpThread.ThreadState)
+            {
+                case ThreadState.Unstarted:
+                    {
+                        Console.WriteLine("tcpThread start");
+                        button1_startService.Text = "Stop";
+                        isServiceStart = true;
+                        ReLoad_Setting();
+                        tcpThread.Start();
+                    }
+                    break;
+
+                case ThreadState.Aborted:
+                case ThreadState.Stopped:
+                    if (button1_startService.Text == "Start")
+                    {
+                        Console.WriteLine("tcpThread start");
+                        button1_startService.Text = "Stop";
+                        tcpThread = new Thread(tcpServer);
+                        isServiceStart = true;
+                        ReLoad_Setting();
+                        tcpThread.Start();
+                    }
+                    break;
+
+                case ThreadState.Running:
+                    if (button1_startService.Text == "Stop")
+                    {
+                        Console.WriteLine("tcpThread stop");
+                        button1_startService.Text = "Start";
+                        isServiceStart = false;
+                        listener.Stop();
+                    }
+                    break;
+            }
+        }
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("get local IP address : " + ip);
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void Form1_Closed(object sender, System.EventArgs e)
+        {
+            //startServiceFlag = false;
+        }
+
+    }
+}
